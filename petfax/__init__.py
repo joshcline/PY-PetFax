9c1119be563a961aef80acd281830c7a7e3eefdf
Josh Cline <joshccline@outlook.com>
2023-08-22 16:32:23 -0400

    Added index.html to facts folder.

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,16 +1,22 @@
+# config                    
 from flask import Flask
 
-#factory
+# factory
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/")
-    def hello():
-        return "Hello, PetFax"
-    
-    #register pet blueprint
-    from . import pet
+    # index route
+    @app.route('/')
+    def index(): 
+        return 'Hello, PetFax!'
+
+    # register pet blueprint 
+    from . import pet 
     app.register_blueprint(pet.bp)
 
-    #return the app
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    # return the app 
     return app
